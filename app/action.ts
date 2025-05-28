538495be7defe9d692360ce1a62d68cabe0eb2bd
cuongmpdev <cuongmp.dev@gmail.com>
2025-05-28 21:24:07 +0700

    update code

--- app/action.ts
+++ app/action.ts
@@ -1,24 +1,24 @@
 "use server";
 
 const googleScriptURL = process.env.GOOGLESCRIPTURL as string;
-type Province = { code: number; name: string };
-type District = { code: number; name: string };
-type Ward = { code: number; name: string };
+// type Province = { code: number; name: string };
+// type District = { code: number; name: string };
+// type Ward = { code: number; name: string };
 
 export const addRegistration = async (
-  form: {
-    tempProvince?: Province;
-    tempDistrict?: District;
-    tempWard?: Ward;
-  },
-  formData: FormData
+  // form: {
+  //   tempProvince?: Province;
+  //   tempDistrict?: District;
+  //   tempWard?: Ward;
+  // },
+  // formData: FormData
 ) => {
-  const companyName = formData.get("companyName") as string;
-  const fullName = formData.get("fullName") as string;
-  const phone = formData.get("phone") as string;
-  const moneyUsed = formData.get("moneyUsed") as string;
-  const budget = formData.get("budget") as string;
-  const { tempProvince, tempDistrict, tempWard } = form;
+  // const companyName = formData.get("companyName") as string;
+  // const fullName = formData.get("fullName") as string;
+  // const phone = formData.get("phone") as string;
+  // const moneyUsed = formData.get("moneyUsed") as string;
+  // const budget = formData.get("budget") as string;
+  // const { tempProvince, tempDistrict, tempWard } = form;
   try {
     await fetch(googleScriptURL, {
       method: "POST",
@@ -26,14 +26,14 @@ export const addRegistration = async (
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        companyName,
-        fullName,
-        phone,
-        province: tempProvince?.name,
-        district: tempDistrict?.name,
-        ward: tempWard?.name,
-        moneyUsed,
-        budget,
+        companyName: "Công ty ABC",
+        fullName: "Nguyễn Văn A",
+        phone: "0123456789",
+        province: "Hà Nội",
+        district: "Cầu Giấy",
+        ward: "Dịch Vọng",
+        moneyUsed: "5000000",
+        budget: "10000000",
       }),
     });
   } catch (error) {
