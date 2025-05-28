639e3b42310f9a25d0c9225a07e0780b22f1b07f
cuongmpdev <cuongmp.dev@gmail.com>
2025-05-28 22:09:07 +0700

    update code

--- app/action.ts
+++ app/action.ts
@@ -13,18 +13,32 @@ export const addRegistration = async (
   },
   formData: FormData
 ) => {
+  const companyName = formData.get("companyName") as string;
   const fullName = formData.get("fullName") as string;
+  const phone = formData.get("phone") as string;
+  const moneyUsed = formData.get("moneyUsed") as string;
+  const budget = formData.get("budget") as string;
+  const { tempProvince, tempDistrict, tempWard } = form;
+
+  const body = {
+        companyName,
+        fullName,
+        phone,
+        province: tempProvince?.name,
+        district: tempDistrict?.name,
+        ward: tempWard?.name,
+        moneyUsed,
+        budget,
+  };
   try {
     await fetch(googleScriptURL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        fullName,
-      }),
+      body: JSON.stringify(body),
     });
   } catch (error) {
-    console.log(error);
+    console.log("error", error);
   }
 };
